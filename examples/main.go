8ecf073d3b7dc24111b0fce7b07b8230ee941093
Mark Smith <mark@theflyingcodr.dev>
2021-02-18 09:23:34 +0000

    initial commit

---
+++ examples/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/theflyingcodr/errs"
+)
+
+func main() {
+	e := errors.Wrap(fmt.Errorf("%w test1", errors.Wrap(lathos.NewErrNotFound("E404", "could not find thing"), "another")), "test1")
+	fmt.Println(errors.Unwrap(e))
+	fmt.Println(lathos.IsNotFound(e))
+	fmt.Println(lathos.IsClientError(e))
+
+	fmt.Printf("%+v\n", Test())
+}
+
+func Test() error {
+	return errors.Wrap(fmt.Errorf("%w test1", errors.Wrap(lathos.NewErrNotFound("E404", "could not find thing"), "another")), "test1")
+}
